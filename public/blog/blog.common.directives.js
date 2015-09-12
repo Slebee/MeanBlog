20509cd582308d32f28b7134c948a31112f6b61f
Slebee <365456507@qq.com>
2015-09-12 16:45:05 +0800

    ss

--- public/blog/blog.common.directives.js
+++ public/blog/blog.common.directives.js
@@ -2,7 +2,7 @@ angular.module('blog').directive('lightBox',function(){
    return {
        restrict:'EA',
        replace:true,
-       template:'<section class="light-box" ng-show="commonServices.lightBoxShow"></section>',
+       template:'<section class="light-box" ng-show="commonServices.lightBoxShow"  ng-class="{true:\'animated sFadeIn\',false:\'animated sFadeOut\'}[commonServices.lightBoxShow]" ></section>',
        controller:['$scope','commonServices',function($scope,commonServices){
             $scope.commonServices = commonServices;
        }]
@@ -13,12 +13,18 @@ angular.module('blog').directive('close',function(){
     return {
         restrict:'EA',
         replace:true,
-        template:'<a class="close" ng-href="/#!/" ng-click="commonServices.hideLightBox();"></a>',
-        controller:['$scope','commonServices',function($scope,commonServices){
+        template:'<a class="close" ng-href="/#!/"></a>',
+        controller:['$scope','commonServices','$timeout',function($scope,commonServices,$timeout){
             $scope.commonServices = commonServices;
         }],
         link:function(scope,element,attrs){
-
+            /*$(window).off().on('hashchange',function(){
+                var urlReg = /articles/g;
+                console.log( urlReg.test(window.location.href) )
+                if( !urlReg.test(window.location.href) ){
+                   // scope.commonServices.hideLightBox();
+                }
+            })*/
         }
     }
 });
--- public/blog/blog.common.services.js
+++ public/blog/blog.common.services.js
@@ -1,13 +1,6 @@
 angular.module('blog').factory('commonServices',function(){
-    var urlReg = /articles/g;
-    //这里采用了简单的hack，判断url地址上有没有带有articles字符串，有的话就让lightbox显示
+    var reg = /articles/g;
     return{
-        lightBoxShow:urlReg.test(window.location.href),
-        showLightBox : function(){
-            this.lightBoxShow = true;
-        },
-        hideLightBox : function(){
-            this.lightBoxShow = false;
-        }
+        articleDetailisShow:reg.test(window.location.href)
     }
 });
